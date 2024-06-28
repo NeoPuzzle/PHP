66e945b91a8d1f885d24d76150cb57218dc1a16e
rbenites <alrrebe@gmail.com>
2024-06-28 11:36:51 -0500

    php tuto switch

--- PHP-TUTO/hello_world.php
+++ PHP-TUTO/hello_world.php
@@ -520,3 +520,71 @@ if ($ab > 10) {
     echo " but not above 20";
   }
 }
+
+echo "++++++Switch++++++" ."\n";
+
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+
+
+
+$favicolor = "red";
+
+switch ($favicolor) {
+  case "red":
+    echo "Your favorite color is red!";
+  case "blue":
+    "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+
+
+
+$d = 4;
+
+switch ($d) {
+  case 6:
+    echo "Today is Saturday";
+    break;
+  case 0:
+    echo "Today is Sunday";
+    break;
+  default:
+    echo "Looking forward to the Weekend";
+}
+
+$d = 3;
+
+switch ($d) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:  
+    echo "The weeks feels so long!";
+    break;
+  case 6:
+  case 0:
+    echo "Weekends are the best!";
+    break;
+  default:
+    echo "Something went wrong";
+}

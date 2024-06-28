a20ca08748a67131dba8f4a8c211355368ca3865
rbenites <alrrebe@gmail.com>
2024-06-28 11:29:05 -0500

    php tuto operators, conditionals

--- PHP-TUTO/hello_world.php
+++ PHP-TUTO/hello_world.php
@@ -429,17 +429,94 @@ define("cars", [
     "Alfa Romeo",
     "BMW",
     "Toyota"
-  ]);
-  echo cars[0];
+    ]);
+echo cars[0];
 
 
-  echo "++++++++++++" ."\n";
+echo "++++++++++++" ."\n";
 
 
-  define("GREETINGG", "Welcome to W3Schools.com!@#$%");
+define("GREETINGG", "Welcome to W3Schools.com!@#$%");
 
 function myTestt() {
-  echo GREETINGG;
+    echo GREETINGG;
 }
 
 myTestt();
+
+
+echo "++++++Conditional++++++" ."\n";
+
+
+
+if (5 > 3) {
+    echo "Have a good day!";
+}
+
+
+$t = 14;
+
+if ($t < 20) {
+  echo "Have a good dayyyy!";
+}
+
+
+$t = 14;
+
+if ($t == 14) {
+  echo "Have a good dayyy!";
+}
+
+
+$a = 200;
+$b = 33;
+$c = 500;
+
+if ($a > $b && $a < $c ) {
+  echo "Both conditions are true";
+}
+
+
+$a = 5;
+
+if ($a == 2 || $a == 3 || $a == 4 || $a == 5 || $a == 6 || $a == 7) {
+  echo "$a is a number between 2 and 7" ."\n";
+}
+
+
+$t = date("H");
+
+if ($t < "20") {
+  echo "Have a good day!!!" ."\n";
+} else {
+  echo "Have a good nighttt!" ."\n";
+}
+
+
+$t = date("H");
+
+if ($t < "10") {
+  echo "Have a good morning!";
+} elseif ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+
+
+$a = 13;
+
+$b = $a < 10 ? "Hello" : "Good Bye";
+
+echo $b;
+
+$ab = 13;
+
+if ($ab > 10) {
+  echo "Above 10";
+  if ($ab > 20) {
+    echo " and also above 20";
+  } else {
+    echo " but not above 20";
+  }
+}

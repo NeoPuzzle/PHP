8ba0a372895f1cdc2439c038044de67c4e3789fb
rbenites <alrrebe@gmail.com>
2024-07-01 22:41:47 -0500

    php tuto functions

--- PHP-TUTO/hello_world.php
+++ PHP-TUTO/hello_world.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 echo "Hola, PHP \n";
 
@@ -754,5 +754,110 @@ foreach ($colors as $x) :
   echo "$x <br>";
 endforeach;
   echo " **" ."\n";
+  function myMessage() {
+    echo "Hello world!";
+  }
+
+  myMessage();
+  echo " **" ."\n";
+
+  function familyName($fname) {
+    echo "$fname Refsnes ";
+  }
+  
+  familyName("Jani");
+  familyName("Hege");
+  familyName("Stale");
+  familyName("Kai Jim");
+  familyName("Borge");
+  echo " **" ."\n";
+
+  function familyNames($fname, $year) {
+    echo "$fname Refsnes. Born in $year <br> ";
+  }
+  
+  familyNames("Hege", "1975");
+  familyNames("Stale", "1978");
+  familyNames("Kai Jim", "1983");
+  echo " **" ."\n";
+  function setHeight($minheight = 50) {
+    echo "The height is : $minheight <br>";
+  }
+  
+  setHeight(350);
+  setHeight(); // will use the default value of 50
+  setHeight(135);
+  setHeight(80);
+  echo " **" ."\n";
+
+  function sum($x, $y) {
+    $z = $x + $y;
+    return $z;
+  }
+  
+  echo "5 + 10 = " . sum(5, 10) . "<br>";
+  echo "7 + 13 = " . sum(7, 13) . "<br>";
+  echo "2 + 4 = " . sum(2, 4);
+  echo " **" ."\n";
+  function add_five(&$value) {
+    $value += 5;
+  }
+  
+  $num = 2;
+  add_five($num);
+  echo $num;
+  echo " **" ."\n";
+
+  function sumMyNumbers(...$x) {
+    $n = 0;
+    $len = count($x);
+    for($i = 0; $i < $len; $i++) {
+      $n += $x[$i];
+    }
+    return $n;
+  }
+  
+  $a = sumMyNumbers(5, 2, 6, 2, 7, 7);
+  echo $a;
+  echo " **" ."\n";
+
+  function myFamily($lastname, ...$firstname) {
+    $txt = "";
+    $len = count($firstname);
+    for($i = 0; $i < $len; $i++) {
+      $txt = $txt."Hi, $firstname[$i] $lastname.<br>";
+    }
+    return $txt;
+  }
+  
+  $a = myFamily("Doe", "Jane", "John", "Joey");
+  echo $a;
+  echo " **" ."\n";
+//   function addNumbers(int $a, int $b) {
+//     return $a + $b;
+//   }
+//   echo addNumbers(5, "5 days");
+  // since strict is NOT enabled "5 days" is changed to int(5), and it will return 10
+  echo " **" ."\n";
+  function addNumbers(float $a, float $b) : float {
+  return $a + $b;
+}
+echo addNumbers(1.2, 5.2);
+  echo " **" ."\n";
+
+  function addNumberss(float $a, float $b) : int {
+    return (int)($a + $b);
+  }
+  echo addNumberss(1.2, 5.2);
   echo " **" ."\n";
   echo " **" ."\n";
+  echo " **" ."\n";
+  echo " **" ."\n";
+  echo " **" ."\n";
+  echo " **" ."\n";
+  echo " **" ."\n";
+  echo " **" ."\n";
+  echo " **" ."\n";
+  echo " **" ."\n";
+
+  ?>

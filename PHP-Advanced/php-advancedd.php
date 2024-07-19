0faa545bbdf3d3d7f224afcfff39cd6a3e3a629b
rbenites <alrrebe@gmail.com>
2024-07-19 16:46:43 -0500

    PHP File Open/Read/Close

--- PHP-Advanced/php-advancedd.php
+++ PHP-Advanced/php-advancedd.php
@@ -12,9 +12,33 @@ echo "I have a $color $car" . "\n";
  ?>
 <p>Some text.</p>
 <p>Some more text.</p>
-<?php echo readfile("webdictionary.txt"); ?>
+<?php echo readfile("webdictionary.txt") . "\n"; ?>
 <?php 
-$myfile = fopen("webdict.txt", "r");
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+echo fread($myfile, filesize("webdictionary.txt")) . "<br>";
+fclose($myfile);
+?>
+
+<?php
+$myfiles = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+echo fgets($myfiles) . "<br>";
+fclose($myfiles);
+?>
+
+<?php
+$myfiler = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+while(!feof($myfiler)) {
+  echo "fgets: " . fgets($myfiler) . "<br>";
+}
+fclose($myfiler);
+?>
+
+<?php
+$myfilese = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+while(!feof($myfilese)) {
+  echo "-" . fgetc($myfilese);
+}
+fclose($myfilese);
 ?>
 <?php include 'footer.php';?>
 <!-- <?php include 'noFileExists.php';?> -->

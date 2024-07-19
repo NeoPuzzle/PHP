8f688a2da335d595c2da488eb6e426fea0552968
rbenites <alrrebe@gmail.com>
2024-07-19 15:23:17 -0500

    include file

--- PHP-Advanced/php-advancedd.php
+++ PHP-Advanced/php-advancedd.php
@@ -2,7 +2,21 @@
 <html>
 <body>
 
-    &copy; 2010-<?php echo date("Y");?>
+<div class="menu">
+    <?php include "menu.php"; ?>
+</div>
+
+<h1>Welcome to my home page!</h1>
+<?php include 'vars.php';
+echo "I have a $color $car" . "\n";
+ ?>
+<p>Some text.</p>
+<p>Some more text.</p>
+<?php include 'footer.php';?>
+<!-- <?php include 'noFileExists.php';?> -->
+<!-- <?php require 'noFileExists.php';?> -->
+
+    <!-- &copy; 2010-<?php echo date("Y");?> -->
 
 </body>
 </html>
